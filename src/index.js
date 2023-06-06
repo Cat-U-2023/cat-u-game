6c1b328effa0f24c81a54296f824ca1025891ad1
Fany Martinez <estefania.marhen@gmail.com>
2023-06-06 12:57:38 +0100

    Add minigame
    modified files html, css and js for minigame catJump

--- src/index.js
+++ src/index.js
@@ -1,136 +1,159 @@
-const songs = document.querySelector("audio");
-const musicButton = document.querySelector(".menu img");
-const healthBar = document.getElementById("health");
-const hungerBar = document.getElementById("hunger");
-const funBar = document.getElementById("fun");
-const cleanBar = document.getElementById("clean");
-const alert = document.getElementById("close-button");
-const windowAlert = document.getElementById("window-notice");
-let count = 0;
+// const songs = document.querySelector("audio");
+// const musicButton = document.querySelector(".menu img");
+// const healthBar = document.getElementById("health");
+// const hungerBar = document.getElementById("hunger");
+// const funBar = document.getElementById("fun");
+// const cleanBar = document.getElementById("clean");
+// const alert = document.getElementById("close-button");
+// const windowAlert = document.getElementById("window-notice");
+// let count = 0;
 
-document.querySelector(".food").addEventListener("click", feedCat);
-document.querySelector(".game").addEventListener("click", resetGame);
-document.querySelector(".clean").addEventListener("click", resetPoop);
+// document.querySelector(".food").addEventListener("click", feedCat);
+// document.querySelector(".game").addEventListener("click", resetGame);
+// document.querySelector(".clean").addEventListener("click", resetPoop);
 
-musicButton.addEventListener("click", toggleSound);
-songs.volume = 0.2;
-function toggleSound() {
-  if (count === 0) {
-    count = 1;
-    songs.play();
-    musicButton.src = "img/img-bottons/Musica-On.png";
-  } else {
-    count = 0;
-    songs.pause();
-    musicButton.src = "img/img-bottons/Musica-Off.png";
+// musicButton.addEventListener("click", toggleSound);
+// songs.volume = 0.2;
+// function toggleSound() {
+//   if (count === 0) {
+//     count = 1;
+//     songs.play();
+//     musicButton.src = "img/img-bottons/Musica-On.png";
+//   } else {
+//     count = 0;
+//     songs.pause();
+//     musicButton.src = "img/img-bottons/Musica-Off.png";
+//   }
+// }
+
+// alert.addEventListener("click", closeAlert);
+// function closeAlert() {
+//   windowAlert.style.display = "none";
+// }
+
+// let hunger = 100;
+// let fun = 100;
+// let clean = 100;
+
+// function feedCat() {
+//   hunger = 100;
+//   return hunger;
+// };
+
+// function resetGame() {
+//   fun = 100;
+//   return fun;
+// };
+
+// function resetPoop() {
+//   clean = 100;
+//   return clean;
+// };
+
+// let totalBars;
+// const hungerInterval = 1300;
+// const funInterval = 1400;
+// const cleanInterval = 1600;
+// const healthInterval = (hungerInterval + funInterval + cleanInterval) / 3;
+
+// function decreaseHunger() {
+//   hunger--;
+//   hungerBar.style.width = hunger + "%";
+//   return hunger;
+// }
+// setInterval(decreaseHunger, hungerInterval);
+
+// function decreaseFun() {
+//   fun--;
+//   funBar.style.width = fun + "%";
+//   return fun;
+// }
+// setInterval(decreaseFun, funInterval);
+
+// function decreaseClean() {
+//   clean--;
+//   cleanBar.style.width = clean + "%";
+//   return clean;
+// }
+// setInterval(decreaseClean, cleanInterval);
+
+// // cambio de imágenes gato
+
+// const catImage = document.querySelector(".cat");
+
+// function changeStatus() {
+//   if (hunger <= 70) {
+//     catImage.src = "img/img-status-cat/Cat-U-Hambriento.png";
+//   }
+//   if (fun <= 50) {
+//     catImage.src = "img/img-status-cat/Cat-U-Enfadado.png";
+//   }
+//   if (clean <= 40) {
+//     catImage.src = "img/img-status-cat/Cat-U-Sucio.png";
+//   } else if (hunger > 70 &&
+//              fun > 50 &&
+//              clean > 40) {
+//     catImage.src = "img/img-status-cat/Cat-U-Normal.png";
+//   }
+// }
+
+// // cambio img de botones
+
+// const buttonFood = document.querySelector(".food");
+// const buttonBall = document.querySelector(".game");
+// const buttonSandBox = document.querySelector(".clean");
+
+// function changeButton() {
+//   if (hunger <= 70) {
+//     buttonFood.src = "img/img-bottons/Btn-food.png";
+//   } else {
+//     buttonFood.src = "img/img-bottons/btn-food-2.png";
+//   }
+//   if (fun <= 50) {
+//     buttonBall.src = "img/img-bottons/btn-fun-2.png";
+//   } else {
+//     buttonBall.src = "img/img-bottons/btn-fun.png";
+//   }
+//   if (clean <= 40) {
+//     buttonSandBox.src = "img/img-bottons/Clean-3.webp";
+//   } else {
+//     buttonSandBox.src = "img/img-bottons/Clean.webp";
+//   }
+// }
+
+// // bara de vida
+
+// function decreaseLife() {
+//   totalBars = (decreaseHunger() + decreaseFun() + decreaseClean()) / 3;
+//   totalBars--;
+//   healthBar.style.width = totalBars + "%";
+//   changeStatus();
+//   changeButton();
+// }
+
+// setInterval(decreaseLife, healthInterval);
+
+// decreaseLife();
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innertWidth;
+canvas.height = window.innertHeight;
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    context.fillRect(this.position.x,
+      this.position.y, this.width, this.height);
   }
 }
-
-alert.addEventListener("click", closeAlert);
-function closeAlert() {
-  windowAlert.style.display = "none";
-}
-
-let hunger = 100;
-let fun = 100;
-let clean = 100;
-
-function feedCat() {
-  hunger = 100;
-  return hunger;
-};
-
-function resetGame() {
-  fun = 100;
-  return fun;
-};
-
-function resetPoop() {
-  clean = 100;
-  return clean;
-};
-
-let totalBars;
-const hungerInterval = 1300;
-const funInterval = 1400;
-const cleanInterval = 1600;
-const healthInterval = (hungerInterval + funInterval + cleanInterval) / 3;
-
-function decreaseHunger() {
-  hunger--;
-  hungerBar.style.width = hunger + "%";
-  return hunger;
-}
-setInterval(decreaseHunger, hungerInterval);
-
-function decreaseFun() {
-  fun--;
-  funBar.style.width = fun + "%";
-  return fun;
-}
-setInterval(decreaseFun, funInterval);
-
-function decreaseClean() {
-  clean--;
-  cleanBar.style.width = clean + "%";
-  return clean;
-}
-setInterval(decreaseClean, cleanInterval);
-
-// cambio de imágenes gato
-
-const catImage = document.querySelector(".cat");
-
-function changeStatus() {
-  if (hunger <= 70) {
-    catImage.src = "img/img-status-cat/Cat-U-Hambriento.png";
-  }
-  if (fun <= 50) {
-    catImage.src = "img/img-status-cat/Cat-U-Enfadado.png";
-  }
-  if (clean <= 40) {
-    catImage.src = "img/img-status-cat/Cat-U-Sucio.png";
-  } else if (hunger > 70 &&
-             fun > 50 &&
-             clean > 40) {
-    catImage.src = "img/img-status-cat/Cat-U-Normal.png";
-  }
-}
-
-// cambio img de botones
-
-const buttonFood = document.querySelector(".food");
-const buttonBall = document.querySelector(".game");
-const buttonSandBox = document.querySelector(".clean");
-
-function changeButton() {
-  if (hunger <= 70) {
-    buttonFood.src = "img/img-bottons/Btn-food.png";
-  } else {
-    buttonFood.src = "img/img-bottons/btn-food-2.png";
-  }
-  if (fun <= 50) {
-    buttonBall.src = "img/img-bottons/btn-fun-2.png";
-  } else {
-    buttonBall.src = "img/img-bottons/btn-fun.png";
-  }
-  if (clean <= 40) {
-    buttonSandBox.src = "img/img-bottons/Clean-3.webp";
-  } else {
-    buttonSandBox.src = "img/img-bottons/Clean.webp";
-  }
-}
-
-// bara de vida
-
-function decreaseLife() {
-  totalBars = (decreaseHunger() + decreaseFun() + decreaseClean()) / 3;
-  totalBars--;
-  healthBar.style.width = totalBars + "%";
-  changeStatus();
-  changeButton();
-}
-
-setInterval(decreaseLife, healthInterval);
-
-decreaseLife();
+const player = new Player();
+player.draw();
