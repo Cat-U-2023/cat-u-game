551d23b75fdbd8ff85fa74eb2b8c6190fa70f2f8
Pablo Santana <pablosntojd@gmail.com>
2023-06-19 20:13:50 +0100

    fixed gameplay cat jump

--- src/modules/cat-jump.js
+++ src/modules/cat-jump.js
@@ -2,13 +2,9 @@ const catPlayer = document.querySelector(".character");
 const mummy = document.querySelector(".obstacle");
 const floor = document.querySelector(".floor");
 const bg = document.querySelector(".background-egypt");
-const score = document.querySelector(".score");
 const gameOver = document.querySelector(".game-over");
 
 export function catJump() {
-  let playerScore = 0;
-  let interval = 0;
-
   document.addEventListener("keydown", (start) => {
     if (start.code === "Space") {
       gameOver.style.display = "none";
@@ -16,12 +12,6 @@ export function catJump() {
       mummy.classList.add("obstacle-moving");
       bg.style.animation = "animated-background 80s linear infinite";
       floor.style.animation = "animated-floor 18s linear infinite";
-      playerScore = 0;
-      const scoreCounter = () => {
-        playerScore++;
-        score.innerHTML = `${playerScore}`;
-      };
-      interval = setInterval(scoreCounter, 400);
     }
   });
 
@@ -41,15 +31,15 @@ export function catJump() {
       const mummyLeft = parseInt(getComputedStyle(mummy).getPropertyValue("left"));
       const catBottom = parseInt(getComputedStyle(catPlayer).getPropertyValue("bottom"));
       // detect collision
-      if (mummyLeft <= 160 && mummyLeft >= 140 && catBottom <= 150) {
+      const catRight = (window.innerWidth * 15) / 100;
+      const catLimit = (window.innerHeight * 25) / 100;
+      if (mummyLeft <= catRight && catBottom <= catLimit) {
         // collision
         gameOver.style.display = "block";
         catPlayer.classList.remove("cat-running");
         mummy.classList.remove("obstacle-moving");
         bg.style.animation = "none";
         floor.style.animation = "none";
-        playerScore = 0;
-        clearInterval(interval);
       }
     }, 10);
   };
